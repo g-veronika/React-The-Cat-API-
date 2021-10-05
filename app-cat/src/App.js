bf2b316c809cdda0962197a64fa9c41fba19fd13
g-veronika <g.veronika2021@gmail.com>
2021-10-05 13:05:31 +0200

    fetch cats api

--- app-cat/src/App.js
+++ app-cat/src/App.js
@@ -1,23 +1,27 @@
-
+import {useState, useEffect} from 'react'; 
 import './App.css';
 
 function App() {
+
+  const [dataImage, setDataImage] = useState();
+
+  useEffect(()=> {
+
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => {
+      console.log(response)
+      return response.json();
+    })
+    .then(data => {
+      console.log(data)
+      setDataImage(data[0].url)
+    })
+
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
